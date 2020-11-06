7f9ba666265551082869c5ac8fa60c55195e0a14
응우엔휴중 <huudung@topengnet.com>
2020-11-06 16:29:07 +0700

    1. Very First Commit

---
+++ MVVMTemplate/Models/DemoModel.cs
@@ -0,0 +1,56 @@
+﻿using MvvmFoundation.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVVMTemplate.Models
+{
+    public class DemoModel : ObservableObject
+    {
+        #region Properties
+
+        #region Public
+        public string MyString
+        {
+            get { return myString; }
+            set
+            {
+                if (value.Equals(myString))
+                {
+                    return;
+                }
+
+                myString = value;
+                RaisePropertyChanged("MyString");
+            }
+        }
+
+        public int MyNumber
+        {
+            get { return myNumber; }
+            set
+            {
+                if (value.Equals(myNumber))
+                {
+                    return;
+                }
+
+                myNumber = value;
+                RaisePropertyChanged("MyNumber");
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region Fields
+
+        #region Private
+        private string myString;
+        private int myNumber;
+        #endregion
+
+        #endregion
+    }
+}
---
+++ MVVMTemplate/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,82 @@
+﻿using MvvmFoundation.Wpf;
+using MVVMTemplate.Models;
+using System;
+using System.Windows.Input;
+
+namespace MVVMTemplate.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            TheModel = new DemoModel
+            {
+                MyString = "Binding works!",
+                MyNumber = 0,
+            };
+        }
+        #endregion
+
+        #region Properties
+
+        #region Public
+        public DemoModel TheModel
+        {
+            get
+            {
+                return theModel;
+            }
+            set
+            {
+                if (value.Equals(theModel))
+                {
+                    return;
+                }
+
+                theModel = value;
+                RaisePropertyChanged("TheModel");
+            }
+        }
+
+        public ICommand CountCommand
+        {
+            get
+            {
+                return new RelayCommand(this.OnCountCommand, () => this.CanCount);
+            }
+        }
+        #endregion
+
+        #region Internal
+        internal bool CanCount
+        {
+            get
+            {
+                return true;
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #region Public
+        public void OnCountCommand()
+        {
+            TheModel.MyNumber++;
+        }
+        #endregion
+
+        #endregion
+
+        #region Fields
+
+        #region Private
+        private DemoModel theModel;
+        #endregion
+
+        #endregion
+    }
+}
